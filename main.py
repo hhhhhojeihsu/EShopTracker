ac882b82f6d13e65f04568c945d301cf3cfebbb4
hhhhojeihsu(Workstation) <hhhhojeihsu@gmail.com>
2019-12-26 17:10:32 +0800

    Add Config Parsing

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 
 import argparse
+import configparser
+import sys
 
 def parsearg():
   argparser_ = argparse.ArgumentParser(prog='EshopTracker',
@@ -18,10 +20,23 @@ def parsearg():
 
   return argparser_.parse_args()
 
+def parseconf(filename):
+  config_ = configparser.RawConfigParser()
+  config_.optionxform = str
+  if(len(config_.read(filename)) != 1):
+    print("Cannot read config file '{}'".format(filename), file=sys.stderr)
+    sys.exit(1)
+  result = dict()
+  for shop in config_.sections():
+    result.update({shop: {item: {"url": attribute.split()[0], "target": attribute.split()[1]}} for item, attribute in config_.items(shop)})
+  return result
 
 def main():
   # Parse arugment
   args = parsearg()
+
+  # Parse config file
+  configs = parseconf(args.config)
 
 if __name__ == '__main__':
   main()
