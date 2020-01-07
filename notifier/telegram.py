27226efea588eedf13d893e7cdeb01f76a651739
hchsu <hchsu0426@cs.nctu.edu.tw>
2020-01-07 21:42:21 +0800

    Update telegram.py To Support Exception Sending

--- notifier/telegram.py
+++ notifier/telegram.py
@@ -3,16 +3,17 @@ import sys
 import requests
 
 ## Telegram
-BOT_TOKEN = "12345:somerandomstring" # The token acquired from telegram @BotFather
-CHAT_ID = "-123456789" # Chat that is group/channel/pm that the bot has access to
+BOT_TOKEN = "663535919:AAHM7yBFx-_9YAjtBdtHdhRjNKPF4Vce2po" # The token acquired from telegram @BotFather
+CHAT_ID = "-257997234" # Chat that is group/channel/pm that the bot has access to
 
 def main():
-  if(len(sys.argv) != 4):
-    sys.exit(1)
-  item = sys.argv[1]
-  threshold = sys.argv[2]
-  current = sys.argv[3]
-  text = "Price for '{}' has reached the threshold ({}) you set. Current price is {}".format(item, threshold, current)
+  if(len(sys.argv) == 1):
+    text = "Exception occurred, program update required"
+  elif(len(sys.argv) == 4):
+    item = sys.argv[1]
+    threshold = sys.argv[2]
+    current = sys.argv[3]
+    text = "Price for '{}' has reached the threshold ({}) you set. Current price is {}".format(item, threshold, current)
 
   with requests.session() as s:
     s.get("https://api.telegram.org/bot" + BOT_TOKEN + "/sendMessage?chat_id=" + CHAT_ID + "&text=" + text)
